cb72671e77dd993d68dec8b22cc36730708d7802
Adam Johnson <johnsonadam187@gmail.com>
2021-06-27 21:44:37 +1200

    added scripts

---
+++ constants.py
@@ -0,0 +1,5 @@
+
+
+
+api_key = 'Buw50cp1iDCVR9ErLEgSsn2Y0Yk4ToVH1tbsZxQO2FLJOxdkgKwzTyui4tHx60vl'
+secret_key  = 'UkW9eYi3X47Q0Pi1ZER6OK80vBmzX7cIXtpCqBI4FSGrzQIwyuKC00aPemBHmQ3K'
---
+++ data_cleaning.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+
+def websocket_to_dataframe(socket):
+    pass
