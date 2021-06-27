5fe78335d94c37749531509d6197421a0fa346c7
Adam Johnson <johnsonadam187@gmail.com>
2021-06-27 20:32:32 +1200

    first commit

---
+++ crypto_bot.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from binance import BinanceSocketManager
+from binance.client import Client
+
+api_key = 'Buw50cp1iDCVR9ErLEgSsn2Y0Yk4ToVH1tbsZxQO2FLJOxdkgKwzTyui4tHx60vl'
+secret_key  = 'UkW9eYi3X47Q0Pi1ZER6OK80vBmzX7cIXtpCqBI4FSGrzQIwyuKC00aPemBHmQ3K'
+
+
+client = Client(api_key, secret_key)
+
+
+
+if __name__=="__main__":
+    pass
+
