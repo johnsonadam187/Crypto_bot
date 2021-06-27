0ab59feb9d28cea41fdb6be816e4e4885852bf58
Adam Johnson <johnsonadam187@gmail.com>
2021-06-27 20:36:43 +1200

    second commit

--- crypto_bot.py
+++ crypto_bot.py
@@ -7,8 +7,12 @@ from binance.client import Client
 api_key = 'Buw50cp1iDCVR9ErLEgSsn2Y0Yk4ToVH1tbsZxQO2FLJOxdkgKwzTyui4tHx60vl'
 secret_key  = 'UkW9eYi3X47Q0Pi1ZER6OK80vBmzX7cIXtpCqBI4FSGrzQIwyuKC00aPemBHmQ3K'
 
+def basic_socket_manager():
+    client = Client(api_key, secret_key)
 
-client = Client(api_key, secret_key)
+
+    manager = BinanceSocketManager(client)
+
 
 
 
